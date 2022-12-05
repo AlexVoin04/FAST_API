3a58fd67ee2eeee3c1558b1aa1d9203f1eca1c52
Alex <alexiv.04@mail.ru>
2022-12-05 13:38:23 +0700

    utils

---
+++ fa_learn_app/utils/repository_utils.py
@@ -0,0 +1,20 @@
+import datetime
+import uuid
+from fa_learn_app.models.product import ProductStorage,ProductOut, ProductIn
+
+
+def convert_products_storage_to_out(product : ProductStorage) -> ProductOut:
+    """Производит конвертацию ProductStorage --> ProductOut"""
+
+    tmp_dict:dict = product.dict()
+    tmp_dict.pop("secret_token", None)
+    return ProductOut(**tmp_dict)
+
+def convert_product_in_to_storage(product :ProductIn) -> ProductStorage:
+    """Производит конвертацию ProductIn --> ProductStorage"""
+
+    tmp_dict :dict = product.dict()
+    product_storage = ProductStorage(id = uuid.uuid4(),
+                                    create_at = datetime.datetime.now(),
+                                    **tmp_dict)
+    return product_storage
