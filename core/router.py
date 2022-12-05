036b34d4cb9cf04fe3574fbf93442e3f775cdf08
Alex <alexiv.04@mail.ru>
2022-12-05 12:56:45 +0700

    main

--- core/router.py
+++ core/router.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
 from fa_learn_app.routers import products
 
-def set_riuters(app :FastAPI):
+def set_routers(app :FastAPI):
     app.include_router(product.router,prefix = "", tags=['products'])
